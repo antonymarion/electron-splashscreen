702f8e0201f9bf2ef1812a9c5bbc66508fbb92fc
antonymarion <>
2019-12-12 14:34:54 +0100

    lib

---
+++ lib/splash-screen.d.ts
@@ -0,0 +1,17 @@
+export interface IProps {
+    mainWindow: any;
+    color?: any;
+    icon?: any;
+    width?: number;
+    height?: number;
+    url?: any;
+    image?: any;
+    brand?: any;
+    productName?: any;
+    logo?: any;
+    website?: any;
+    text?: any;
+    backgroundColor?: string;
+}
+export declare const initSplashScreen: ({ mainWindow, color, icon, width, height, url, image, brand, productName, logo, website, text, backgroundColor }: IProps) => () => void;
+//# sourceMappingURL=splash-screen.d.ts.map
